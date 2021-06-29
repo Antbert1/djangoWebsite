53dfe4c2c451c9deec915a7f998ec71387c35a09
Antbert1 <anthea4002@gmail.com>
2021-06-29 20:09:00 +0100

    oops

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -87,18 +87,18 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Zlx990810',
-        'HOST': 'localhost',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'blog_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Zlx990810',
+#         'HOST': 'localhost',
+#     }
+# }
 
 
-# DATABASES = {'default': dj_database_url.config(default='postgres://user:Zlx990810@localhost.blog_db')}
+DATABASES = {'default': dj_database_url.config(default='postgres://user:Zlx990810@localhost.blog_db')}
 
 
 # Password validation
