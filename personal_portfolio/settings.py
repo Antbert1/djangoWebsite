f87f55f93758cca7e4d9998a0241442c95c9d5d1
Antbert1 <anthea4002@gmail.com>
2021-06-29 21:16:22 +0100

    whatever

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path, os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 # STATIC_ROOT = ''
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static')
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = ( os.path.join('static'), )
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
