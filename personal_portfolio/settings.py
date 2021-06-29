34bd5e7c984c51a93f1d8987fd8099ec1f04b4af
Antbert1 <anthea4002@gmail.com>
2021-06-29 18:44:06 +0100

    captcha

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '6hw^*(#fnhd9l(z@e3whix+=^**78(5588*%_vf^#4c-j_msao'
 
-GOOGLE_RECAPTCHA_SECRET_KEY = '6LeuJE4bAAAAADeGHlLZ-E01UfpbqhArJtTn0sdo'
+# GOOGLE_RECAPTCHA_SECRET_KEY = '6LeuJE4bAAAAADeGHlLZ-E01UfpbqhArJtTn0sdo'
+RECAPTCHA_PUBLIC_KEY = '6LeuJE4bAAAAAGeH4czj4lu4PnwzzottMi657MU6'
+RECAPTCHA_PRIVATE_KEY = '6LeuJE4bAAAAADeGHlLZ-E01UfpbqhArJtTn0sdo'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -85,18 +87,18 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Zlx990810',
-        'HOST': 'localhost',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'blog_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Zlx990810',
+#         'HOST': 'localhost',
+#     }
+# }
 
 
-# DATABASES = {'default': dj_database_url.config(default='postgres://user:Zlx990810@localhost.blog_db')}
+DATABASES = {'default': dj_database_url.config(default='postgres://user:Zlx990810@localhost.blog_db')}
 
 
 # Password validation
